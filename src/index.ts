f4d42bdbcf143e067a78be812c04800c24b6faf9
Miguel Carneiro <miguel.carneiro.morenas@everis.com>
2020-10-09 23:47:03 +0100

    Added tests

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,13 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { getGreeting, replaceReadme } from './utils';
 
 const date: Date = new Date();
 
 const readme = fs.readFileSync(path.join(__dirname, '../README.md'), 'utf8');
 
-const newReadme = readme.replace(
-  /[0-9]{2}[/][0-9]{2}[/][0-9]{4}/,
-  `${date.getUTCDate() + 1}/${date.getUTCMonth() + 1}/${date.getUTCFullYear()}`
-);
+let newReadme = replaceReadme(date, readme);
 
-if (date.getUTCHours() == 0) {
-  newReadme.replace('Good Afternoon', 'Good Morning');
-} else {
-  newReadme.replace('Good Morning', 'Good Afternoon');
-}
+newReadme = getGreeting(date, newReadme);
 
 fs.writeFileSync(path.join(__dirname, '../README.md'), newReadme);
